617c9ff6cdce09bbd903a810db31a0ca4a9a2cef
Kaizer Oman <90407667+kaizeroman@users.noreply.github.com>
2025-08-08 21:21:46 +0800

    Anecdotes step 2: Vote functionality done

--- part_1/anecdotes/src/App.jsx
+++ part_1/anecdotes/src/App.jsx
@@ -13,10 +13,20 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  const addVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
 
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={addVote}>
+        Vote
+      </button>
       <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>
         Next Anecdote
       </button>
