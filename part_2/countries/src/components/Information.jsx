1f4e18e24f7eb3cfc972f8e22e7fe154848942fe
Kaizer Oman <90407667+kaizeroman@users.noreply.github.com>
2025-08-25 12:00:22 +0800

    Data for countries step 3 done

--- part_2/countries/src/components/Information.jsx
+++ part_2/countries/src/components/Information.jsx
@@ -1,4 +1,24 @@
+import { useEffect, useState } from "react"
+import getWeather from "../services/Weather"
+
 const Information = ({country}) => {
+    const [weather, setWeather] = useState(null)
+    useEffect(() => {
+        getWeather({location: country.name.common})
+        .then(response => setWeather(response))
+        }, [country]
+    )
+    let tempCel = 0;
+    let image = ''
+    let alt = ''
+    let windSpeed = 0
+    if(weather){
+        tempCel = (Math.round(weather.main.temp - 273.15)*100)/100
+        image = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
+        alt = weather.weather[0].description
+        windSpeed = weather.wind.speed
+    }
+    
     return(
         <>
             <h1>{country.name.common}</h1>
@@ -9,6 +29,13 @@ const Information = ({country}) => {
                 {Object.entries(country.languages).map(([code, name]) => <li key = {code}>{name}</li>)}
             </ul>
             <img src={country.flags.png} alt={country.name.common}/>
+            <h1>Weather in {country.name.common}</h1>
+            <p>Temperature {tempCel} Celsius</p>
+            <img
+                src={image}
+                alt={alt}
+            />
+            <p>Wind {windSpeed} m/s</p>
         </>
     )
 }
--- part_2/countries/src/services/Weather.jsx
+++ part_2/countries/src/services/Weather.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-const getWeather = ({country}) => {
+const getWeather = ({location}) => {
     const api = import.meta.env.VITE_API_KEY
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${api}`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api}`
     const request = axios.get(url)
     return request.then(response => response.data)
 }
