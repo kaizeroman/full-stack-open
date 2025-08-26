8652164f0743bc03787032dd5b5de0da90f3f869
Kaizer Oman <90407667+kaizeroman@users.noreply.github.com>
2025-08-26 22:50:17 +0800

    Phonebook backend step 5

--- part_3/index.js
+++ part_3/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
 let persons = [
     { 
       "id": "1",
@@ -48,6 +49,19 @@ app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
     persons = persons.filter(person => person.id !== id)
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    const person = {
+        id: String(Math.floor(Math.random()*100000000)),
+        name: body.name,
+        number: body.number
+    }
+    
+    persons = persons.concat(person)
+    response.json(person)
 })
 
 const PORT = 3001
