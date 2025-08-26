32b5aeb90cb31dafa4b2dcb42f94cdc2d71b87f3
Kaizer Oman <90407667+kaizeroman@users.noreply.github.com>
2025-08-26 22:50:49 +0800

    Phonebook step 6

--- part_3/index.js
+++ part_3/index.js
@@ -54,6 +54,19 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
+    if(!body.name || !body.number) {
+        return response.status(400).json({
+            error: "name or number is missing"
+        })
+    }
+
+    const nameExists = persons.find(person => person.name === body.name)
+    if (nameExists) {
+        return response.status(400).json({
+            error: `${body.name} already exists`
+        })
+    }
+    
     const person = {
         id: String(Math.floor(Math.random()*100000000)),
         name: body.name,
