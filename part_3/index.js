fbc51cb5fe61d8dfdaefe37e46090958e748ea67
Kaizer Oman <90407667+kaizeroman@users.noreply.github.com>
2025-08-27 01:18:41 +0800

    Phonebook backend step 8

--- part_3/index.js
+++ part_3/index.js
@@ -3,9 +3,14 @@ const morgan = require('morgan')
 
 const app = express()
 
-app.use(morgan('tiny'))
-
 app.use(express.json())
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
 let persons = [
     { 
       "id": "1",
