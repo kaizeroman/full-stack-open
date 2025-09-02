9420b8f9393085376e50bf9e2759176a270baa1f
Kaizer Oman <90407667+kaizeroman@users.noreply.github.com>
2025-09-02 15:31:37 +0800

    Phonebook backend Step 9

--- part_3/index.js
+++ part_3/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
-
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
 morgan.token('body', (req) => JSON.stringify(req.body))
 
 
