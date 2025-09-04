6521aed00099151f1bf0b16144f460767ed2ca8b
Kaizer Oman <90407667+kaizeroman@users.noreply.github.com>
2025-09-04 17:18:55 +0800

    Phonebook database steps 3 & 4

--- part_2/phonebook/src/services/Phonebook.jsx
+++ part_2/phonebook/src/services/Phonebook.jsx
@@ -19,8 +19,7 @@ const deleteContact = (id) => {
 
 const replaceNumber = (id, newContact) => {
     const request = axios.put(`${baseURL}/${id}`, newContact)
-    console.log(`${baseURL}/${id}`)
-    return request
+    return request.then(response => response.data)
 }
 
 export default {
